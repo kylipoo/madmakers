28458955ae6a21c455e8e802e1c1b7be86b34129
kylipoo <kyleyli2005@gmail.com>
2022-08-23 00:39:08 -0700

    change title

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -29,7 +29,7 @@ const Home = () => {
 
   return (
     <div className='App'>
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>Monsters Madlibs</h1>
 
       <SearchBox
         className='monsters-search-box'
