4ae22d3cb45c5c772ff1b30479afd6ce4693bc75
Kyle Li <kyleyli2005@gmail.com>
2022-08-25 11:11:28 -0700

    image loading

--- src/components/madlibs/output.component.jsx
+++ src/components/madlibs/output.component.jsx
@@ -1,7 +1,13 @@
 import "./madlibs.styles.css";
 import story from "./output";
+import { useState, useEffect } from 'react';
 const Output = ({ fields, id, word}) => {
-  const theRandomNumber = Math.floor(Math.random() * 500) + 1;
+  const [theRandomNumber, setTheRandomeNumber] = useState(1);
+  const [group, setGroup] = useState(1);
+  useEffect(() => {
+    setTheRandomeNumber(Math.floor(Math.random() * 500) + 1);
+    setGroup(Math.floor(Math.random() * 4) + 1);
+  }, []);
   return (
     <div  className="outputContainer">
       <div dangerouslySetInnerHTML={{ __html: story(fields, id) }} />
@@ -11,7 +17,7 @@ const Output = ({ fields, id, word}) => {
           <img src='http://www.downloadclipart.net/medium/speech-bubble-png-file.png' alt="speech bubble" />
         </div>
         <div className="robot">
-          <img src={`https://robohash.org/${theRandomNumber}?set=set2&size=180x180`}  alt="robot"/>
+          <img src={`https://robohash.org/${theRandomNumber}?set=set${group}&size=180x180`}  alt="robot"/>
         </div>
       </div>
     </div>
