02dd94d0ff74ac19ce7f136be9e679c5f0a444a7
Kyle Li <kyleyli2005@gmail.com>
2022-08-25 09:36:23 -0700

    format for the output

--- src/components/madlibs/output.component.jsx
+++ src/components/madlibs/output.component.jsx
@@ -1,62 +1,25 @@
 import "./madlibs.styles.css";
+import {story1, story2, story3, story4, story5, story6} from "./output";
 const Output = ({ fields, id }) => {
   return (
     <>
-      {id === 1 && (
-        <div>
-          {`Sometimes I forget to wash my (  ${fields[0].value}  ), and it ends up smelling like (  ${fields[1].value}  ), 
-            Sometimes I forgot to clean my (  ${fields[2].value}  ), too, and then my (${fields[3].value}) really smells like a (  ${fields[4].value}  ),
-            after a long day of (  ${fields[5].value}  ) and farting. Oh man, I should really clean up better! Sometimes I find a (  ${fields[6].value}  )
-            in my shoe after playing (  ${fields[7].value}  ) for too long. And sometimes I find (  ${fields[8].value}  ) poop under my shoe, 
-            and I get (  ${fields[9].value}  ) smells all over my mom's (  ${fields[10].value}  ).`}
-        </div>
-      )}
+      {id === 1 &&
+        <div dangerouslySetInnerHTML={{ __html: story1(fields)}}/>
+      }
       {id === 2 && (
-        <div>
-          {`Is your (  ${fields[0].value} ) full of (  ${fields[1].value} ) food? 
-            Maybe you should buy the (  ${fields[2].value}  ) in the cafeteria. The sandwiches are made with (  ${fields[3].value}  ) ( ${fields[4].value}  ).
-            Yum. They are wrapped in (  ${fields[5].value}  ) and are very (  ${fields[6].value}   )
-            But if you pack your own lunch, be sure to bring (  ${fields[7].value} ) (  ${fields[8].value}  ). Everyone loves those.
-            You can even share them with  (  ${fields[9].value}  ).`}
-        </div>
+         <div dangerouslySetInnerHTML={{ __html: story2(fields) }} />
       )}
       {id === 3 && (
-        <div>
-          {`There is all types of pee. (  ${fields[0].value}  ) pee,  (  ${fields[1].value}  ) pee,  
-            (  ${fields[2].value}  ) pee, and even (  ${fields[3].value}  ) pee. Everyone pees! Some pee is (  ${fields[4].value}  ).
-            Some pee smells like (  ${fields[5].value}  ). Some pee flows like the (  ${fields[6].value}  ), while some pee drips like a
-            (  ${fields[7].value}  ). Some people (  ${fields[8].value}  ) while they pee. Others simply stand and (  ${fields[9].value}  ) their
-            head while they pee. Just don't be a (  ${fields[10].value}  ) person who gets pee all over the (  ${fields[11].value}  )!
-            `}
-        </div>
+        <div dangerouslySetInnerHTML={{ __html: story3(fields) }} />
       )}
       {id === 4 && (
-        <div>
-          {`Today I went to the zoo. I saw a(n) (  ${fields[0].value}  ) (  ${fields[1].value}  ) jumping up and down in its tree.  He  
-            (  ${fields[2].value}  ) (  ${fields[3].value}  ) through the large tunnel that led to its (  ${fields[4].value}  )
-            (  ${fields[5].value}  ). I got some peanuts and passed them through the cage to a gigantic gray (  ${fields[6].value}  ) towering above my head. 
-            Feeding that animal made me hungry. I went to get a (  ${fields[7].value}  ) scoop of ice cream. It filled my stomach. Afterwards I had to (  ${fields[8].value}  ) while they pee. Others simply stand and (  ${fields[9].value}  ) their
-            (  ${fields[9].value}  )to catch our bus. When I got home I (  ${fields[10].value}  ) my mom for a (  ${fields[11].value}  ) day at the zoo.
-            `}
-        </div>
+        <div dangerouslySetInnerHTML={{ __html: story4(fields) }} />
       )}
       {id === 5 && (
-        <div>
-          {`I, the (  ${fields[0].value}  ) and (  ${fields[1].value}  ) (  ${fields[2].value}  ) has (  ${fields[3].value}  )  (  ${fields[4].value}  )'s sister and plans to steal her
-            (  ${fields[5].value}  )(  ${fields[6].value}  )! What are a (  ${fields[7].value}  ) and backpacking (  ${fields[8].value}  )to do? Before you can help (  ${fields[9].value}  ), you'll have to collect
-            the (  ${fields[9].value}  )(  ${fields[10].value}  ) and (  ${fields[11].value}  )(  ${fields[12].value}  ) that open up the (  ${fields[12].value}  ) worlds connected to a
-            (  ${fields[13].value}  ) lair. There are (  ${fields[14].value}  )(  ${fields[15].value}  ) and (  ${fields[16].value}  )(  ${fields[17].value}  ) in the game, along with hundreds of other goodies for you to find.
-            `}
-        </div>
+        <div dangerouslySetInnerHTML={{ __html: story5(fields) }} />
       )}
       {id === 6 && (
-        <div>
-          {`I walk through the color jungle. I take out my (  ${fields[0].value}  ) canteen. There's a (  ${fields[1].value}  ) parrot with a (  ${fields[2].value}  ) (  ${fields[3].value}  ) in his mouth right there in front of me in the (  ${fields[4].value}  ) trees! I gaze at his 
-          (  ${fields[5].value}  ) (  ${fields[6].value}  ). A sudden sound awakes me from my daydream! A panther's (  ${fields[7].value}  ) in front of my head! I
-          (  ${fields[8].value}  ) his (  ${fields[9].value}  ) breath. I remember I have a packet of (  ${fields[10].value}  ) that makes go into a deep slumber! I (  ${fields[11].value}  ) it away from me in front of the (  ${fields[12].value}  ). Yes he's eating it! I 
-          (  ${fields[13].value}  ) away through the (  ${fields[14].value}  ) jungle. I meet the parents at the tent. Phew! It's been an exciting day in the jungle.
-            `}
-        </div>
+        <div dangerouslySetInnerHTML={{ __html: story6(fields) }} />
       )}
     </>
   );
---
+++ src/components/madlibs/output.js
@@ -0,0 +1,38 @@
+export const story1 = (fields) =>
+    `Sometimes I forget to wash my <span>(  ${fields[0].value}  )</span>, and it ends up smelling like <span>(  ${fields[1].value}  )</span>, 
+Sometimes I forgot to clean my <span>(  ${fields[2].value}  )</span>, too, and then my (${fields[3].value}) really smells like a <span>(  ${fields[4].value}  )</span>,
+after a long day of <span>(  ${fields[5].value}  )</span> and farting. Oh man, I should really clean up better! Sometimes I find a <span>(  ${fields[6].value}  )</span>
+in my shoe after playing <span>(  ${fields[7].value}  )</span> for too long. And sometimes I find <span>(  ${fields[8].value}  )</span> poop under my shoe, 
+and I get <span>(  ${fields[9].value}  )</span> smells all over my mom's <span>(  ${fields[10].value}  )</span>.`;
+export const story2 = (fields) =>
+    `Is your <span>(  ${fields[0].value} )</span> full of <span>(  ${fields[1].value} )</span> food? 
+        Maybe you should buy the <span>(  ${fields[2].value}  )</span> in the cafeteria. The sandwiches are made with <span>(  ${fields[3].value}  )</span> ( ${fields[4].value}  )</span>.
+        Yum. They are wrapped in <span>(  ${fields[5].value}  )</span> and are very <span>(  ${fields[6].value}   )</span>
+        But if you pack your own lunch, be sure to bring <span>(  ${fields[7].value} )</span><span>(  ${fields[8].value}  )</span>. Everyone loves those.
+        You can even share them with  <span>(  ${fields[9].value}  )</span>.`
+export const story3 = (fields) =>
+    `There is all types of pee. <span>(  ${fields[0].value}  )</span> pee,  <span>(  ${fields[1].value}  )</span> pee,  
+<span>(  ${fields[2].value}  )</span> pee, and even <span>(  ${fields[3].value}  )</span> pee. Everyone pees! Some pee is <span>(  ${fields[4].value}  )</span>.
+Some pee smells like <span>(  ${fields[5].value}  )</span>. Some pee flows like the <span>(  ${fields[6].value}  )</span>, while some pee drips like a
+<span>(  ${fields[7].value}  )</span>. Some people <span>(  ${fields[8].value}  )</span> while they pee. Others simply stand and <span>(  ${fields[9].value}  )</span> their
+head while they pee. Just don't be a <span>(  ${fields[10].value}  )</span> person who gets pee all over the <span>(  ${fields[11].value}  )</span>!
+`;
+export const story4 = (fields) =>
+    `Today I went to the zoo. I saw a(n) <span>(  ${fields[0].value}  )</span> <span>(  ${fields[1].value}  )</span> jumping up and down in its tree.  He  
+<span>(  ${fields[2].value}  )</span> <span>(  ${fields[3].value}  )</span> through the large tunnel that led to its <span>(  ${fields[4].value}  )</span>
+<span>(  ${fields[5].value}  )</span>. I got some peanuts and passed them through the cage to a gigantic gray <span>(  ${fields[6].value}  )</span> towering above my head. 
+Feeding that animal made me hungry. I went to get a <span>(  ${fields[7].value}  )</span> scoop of ice cream. It filled my stomach. Afterwards I had to <span>(  ${fields[8].value}  )</span> while they pee. Others simply stand and <span>(  ${fields[9].value}  )</span> their
+<span>(  ${fields[9].value}  )</span>to catch our bus. When I got home I <span>(  ${fields[10].value}  )</span> my mom for a <span>(  ${fields[11].value}  )</span> day at the zoo.
+`;
+export const story5 = (fields) =>
+    `I, the <span>(  ${fields[0].value}  )</span> and <span>(  ${fields[1].value}  )</span> <span>(  ${fields[2].value}  )</span> has <span>(  ${fields[3].value}  )</span>  <span>(  ${fields[4].value}  )</span>'s sister and plans to steal her
+    <span>(  ${fields[5].value}  )</span><span>(  ${fields[6].value}  )</span>! What are a <span>(  ${fields[7].value}  )</span> and backpacking <span>(  ${fields[8].value}  )</span>to do? Before you can help <span>(  ${fields[9].value}  )</span>, you'll have to collect
+    the <span>(  ${fields[9].value}  )</span><span>(  ${fields[10].value}  )</span> and <span>(  ${fields[11].value}  )</span><span>(  ${fields[12].value}  )</span> that open up the <span>(  ${fields[12].value}  )</span> worlds connected to a
+    <span>(  ${fields[13].value}  )</span> lair. There are <span>(  ${fields[14].value}  )</span><span>(  ${fields[15].value}  )</span> and <span>(  ${fields[16].value}  )</span><span>(  ${fields[17].value}  )</span> in the game, along with hundreds of other goodies for you to find.
+`;
+export const story6 = (fields) =>
+`I walk through the color jungle. I take out my <span>(  ${fields[0].value}  )</span> canteen. There's a <span>(  ${fields[1].value}  )</span> parrot with a <span>(  ${fields[2].value}  )</span> <span>(  ${fields[3].value}  )</span> in his mouth right there in front of me in the <span>(  ${fields[4].value}  )</span> trees! I gaze at his 
+<span>(  ${fields[5].value}  )</span> <span>(  ${fields[6].value}  )</span>. A sudden sound awakes me from my daydream! A panther's <span>(  ${fields[7].value}  )</span> in front of my head! I
+<span>(  ${fields[8].value}  )</span> his <span>(  ${fields[9].value}  )</span> breath. I remember I have a packet of <span>(  ${fields[10].value}  )</span> that makes go into a deep slumber! I <span>(  ${fields[11].value}  )</span> it away from me in front of the <span>(  ${fields[12].value}  )</span>. Yes he's eating it! I 
+<span>(  ${fields[13].value}  )</span> away through the <span>(  ${fields[14].value}  )</span> jungle. I meet the parents at the tent. Phew! It's been an exciting day in the jungle.
+  `;
