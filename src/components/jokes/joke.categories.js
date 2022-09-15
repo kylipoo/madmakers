bb40b8d806fdfd603536039128469ca8f62a5fff
kylipoo <kyleyli2005@gmail.com>
2022-09-15 16:59:17 -0700

    Am implementing list of card for joke categories.

---
+++ src/components/jokes/joke.categories.js
@@ -0,0 +1,13 @@
+export const jokeCategories = [
+    {
+        id: 1,
+        name: 'Knock-knock',
+        description: 'Who is there?'
+    },
+    {
+        id: 2,
+        name: 'Puns',
+        description: 'Play on words'
+    },
+    
+]
