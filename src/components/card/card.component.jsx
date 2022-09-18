b72b65cc628720f562d1e69c1264f7d1c0634374
kylipoo <kyleyli2005@gmail.com>
2022-09-18 15:52:15 -0700

    Created separate links for madlibs and jokes

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,18 +4,34 @@ import { Link } from "react-router-dom";
 const Card = ({ monster }) => {
   const { id, name, description } = monster;
 
+  if(name === 'Mad libs') {
   return (
     <div className="card-container">
-      <Link to={`/jokes`}>
+      <Link to={`/madlibs`}>
         <img
           alt={`monster ${name}`}
           src={`https://robohash.org/${id}?set=set2&size=180x180`}
         />
         <h2>{name}</h2>
-        <p>{description}</p>
       </Link>
+      <p>{description}</p>
     </div>
   );
+  } 
+  else  {
+    return (
+      <div className="card-container">
+        <Link to={`/jokes`}>
+          <img
+            alt={`monster ${name}`}
+            src={`https://robohash.org/${id}?set=set2&size=180x180`}
+          />
+          <h2>{name}</h2>
+        </Link>
+        <p>{description}</p>
+      </div>
+    );
+    }
 };
 
 export default Card;
